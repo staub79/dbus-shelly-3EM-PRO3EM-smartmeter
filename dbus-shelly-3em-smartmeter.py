8065bdd09013c5761157fe5f9b187c24d32d3c6f
Michael <michael@bartsch2010.de>
2025-02-11 12:51:09 +0100

    Update dbus-shelly-3em-smartmeter.py

--- dbus-shelly-3em-smartmeter.py
+++ dbus-shelly-3em-smartmeter.py
@@ -195,8 +195,6 @@ class DbusShelly3emService:
     try:
       #get data from Shelly 3em
       meter_data = self._getShellyData()
-      config = self._getConfig()
-      ShellyType = config['ONPREMISE']['ShellyType']
 
       try:
         remapL1 = int(config['ONPREMISE']['L1Position'])
@@ -208,6 +206,8 @@ class DbusShelly3emService:
         meter_data['emeters'][0] = meter_data['emeters'][remapL1-1]
         meter_data['emeters'][remapL1-1] = old_l1
 
+        config = self._getConfig()
+        ShellyType = config['ONPREMISE']['ShellyType']
         if ShellyType == 'Shelly3EM':
             #send data to DBus
             self._dbusservice['/Ac/Power'] = meter_data['total_power']
@@ -244,8 +244,8 @@ class DbusShelly3emService:
             self._dbusservice['/Ac/L1/Energy/Reverse'] = (meter_data['emdata:0']['a_total_act_ret_energy']/1000) 
             self._dbusservice['/Ac/L2/Energy/Reverse'] = (meter_data['emdata:0']['b_total_act_ret_energy']/1000) 
             self._dbusservice['/Ac/L3/Energy/Reverse'] = (meter_data['emdata:0']['c_total_act_ret_energy']/1000) 
-      else:
-          raise ValueError(f"Unsupported ShellyType: {ShellyType}")
+          else:
+              raise ValueError(f"Unsupported ShellyType: {ShellyType}")
         
       # Old version
       #self._dbusservice['/Ac/Energy/Forward'] = self._dbusservice['/Ac/L1/Energy/Forward'] + self._dbusservice['/Ac/L2/Energy/Forward'] + self._dbusservice['/Ac/L3/Energy/Forward']
